69301efdf4eecbf4c7dd1a434226b23713755163
Erin Dachtler <download333@gmail.com>
2016-06-22 14:15:56 -0700

    Ready to add test

---
+++ test/index.js
@@ -0,0 +1,34 @@
+const jsdom = require('mocha-jsdom');
+
+
+describe('Component', () => {
+	jsdom();
+
+	xit('can be created from a function', () => {
+		// TODO: simple
+	});
+
+	xit('can be created from a spec with state', () => {
+		// TODO: counter
+	});
+
+	xit('can be created with lifecycle', () => {
+		// TODO: clock
+	});
+
+	xit('can respond to props changes', () => {
+		// TODO: simple
+	});
+
+	xit('can respond to attribute changes', () => {
+		// TODO: simple
+	});
+
+	xit('can respond to state changes', () => {
+		// TODO: counter
+	});
+
+	xit('can respond to lifecycle notifications', () => {
+		// TODO: clock
+	});
+});
